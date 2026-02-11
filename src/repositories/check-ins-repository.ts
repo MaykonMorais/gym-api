d963810efe1a713e40ad93a483f7117c22080046
MaykonMorais <maykon.morais@protonmail.com>
2026-02-10 21:42:44 -0300

    feat: add validate check-in use case

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -2,8 +2,10 @@ import { CheckIn } from "generated/prisma/client";
 import { CheckInUncheckedCreateInput } from "generated/prisma/models";
 
 export interface CheckInsRepository {
+  findById(id: string): Promise<CheckIn | null>;
   countByUserId(userId: string): Promise<number>;
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>;
   create(data: CheckInUncheckedCreateInput): Promise<CheckIn>;
+  save(checkIn: CheckIn): Promise<CheckIn>;
   findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>;
 }
