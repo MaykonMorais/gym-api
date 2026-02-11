7d4731680bc82d61f49eec0dc8a0983bc5faf99b
MaykonMorais <maykon.morais@protonmail.com>
2026-02-11 20:49:08 -0300

    feat: validate check-in until 20 minutes

--- src/use-cases/validate-check-in.ts
+++ src/use-cases/validate-check-in.ts
@@ -1,6 +1,9 @@
+import dayjs from "dayjs";
+
 import { CheckInsRepository } from "@/repositories/check-ins-repository";
 import { CheckIn } from "generated/prisma/client";
 import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+import { LateCheckInValidationError } from "./errors/late-check-in-validation-error";
 
 interface ValidateCheckInUseCaseRequest {
   checkInId: string;
@@ -20,6 +23,15 @@ export class ValidateCheckInUseCase {
 
     if (!checkIn) {
       throw new ResourceNotFoundError();
+    }
+
+    const distanceInMinutesFromCheckInCreation = dayjs(new Date()).diff(
+      checkIn.created_at,
+      "minutes",
+    );
+
+    if (distanceInMinutesFromCheckInCreation > 20) {
+      throw new LateCheckInValidationError();
     }
 
     checkIn.validated_at = new Date();
