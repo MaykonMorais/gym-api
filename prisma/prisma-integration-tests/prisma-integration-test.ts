e80bc36e5cee998445381a2cc9aa26650adf29bd
Maykon Morais <maykon.silva@compasso.com.br>
2026-02-16 12:39:32 -0300

    feat: add register e2e test

---
+++ prisma/prisma-integration-tests/prisma-integration-test.ts
@@ -0,0 +1,37 @@
+import { Environment } from "vitest/environments";
+
+import { prisma } from "@/libs/prisma";
+import { randomUUID } from "node:crypto";
+import { execSync } from "node:child_process";
+
+const generateDatabaseUrl = (uuid: string) => {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("Please provide a DATABASE_URL environment variable");
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set("schema", uuid);
+
+  return url.toString();
+};
+
+export default <Environment>{
+  name: "prisma",
+  viteEnvironment: "ssr",
+  setup: async () => {
+    const id = randomUUID();
+    const databaseUrl = generateDatabaseUrl(id);
+
+    process.env.DATABASE_URL = databaseUrl;
+
+    execSync("npx prisma db push");
+
+    return {
+      teardown: async () => {
+        await prisma.$executeRawUnsafe(`DROP SCHEMA IF SCHEMA ${id} CASCADE`);
+
+        await prisma.$disconnect();
+      },
+    };
+  },
+};
--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,25 @@ import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsConfigPaths()],
+  test: {
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: "unit",
+          include: ["./src/use-cases/*.spec.ts"],
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: "e2e",
+          include: ["./src/http/controllers/*.spec.ts"],
+          setupFiles: [
+            "./prisma/prisma-integration-tests/prisma-integration-test.ts",
+          ],
+        },
+      },
+    ],
+  },
 });
