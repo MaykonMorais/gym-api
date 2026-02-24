adf63c97033ba1c79a489eb5535de4d9141c83de
MaykonMorais <maykon.morais@protonmail.com>
2026-02-23 21:40:10 -0300

    feat: add check-ins controller

---
+++ src/http/controllers/check-ins/create.ts
@@ -0,0 +1,31 @@
+import { makeCheckInUseCase } from "@/use-cases/factories/make-check-in-use-case";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function create(request: FastifyRequest, response: FastifyReply) {
+  const createCheckInParamSchema = z.object({
+    gymId: z.uuid(),
+  });
+
+  const createCheckInBodySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90;
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180;
+    }),
+  });
+
+  const { gymId } = createCheckInParamSchema.parse(request.params);
+  const { latitude, longitude } = createCheckInBodySchema.parse(request.body);
+
+  const checkInUseCase = makeCheckInUseCase();
+  await checkInUseCase.execute({
+    gymId,
+    userId: request.user.sub,
+    userLatitude: latitude,
+    userLongitude: longitude,
+  });
+
+  return response.status(201).send();
+}
