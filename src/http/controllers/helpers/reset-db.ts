6eed3fdcda45dd975a150e621aacc43dbafba92d
MaykonMorais <maykon.morais@protonmail.com>
2026-03-03 20:47:02 -0300

    fix(e2e): change order of delete tables

--- src/http/controllers/helpers/reset-db.ts
+++ src/http/controllers/helpers/reset-db.ts
@@ -2,8 +2,8 @@ import { prisma } from "@/libs/prisma";
 
 export default async () => {
   await prisma.$transaction([
-    prisma.user.deleteMany(),
-    prisma.gym.deleteMany(),
     prisma.checkIn.deleteMany(),
+    prisma.gym.deleteMany(),
+    prisma.user.deleteMany(),
   ]);
 };
